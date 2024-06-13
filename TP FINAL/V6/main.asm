d23e13aa0040b271b577f5905cb57cb8714d9617
brianm05 <146150029+brianm05@users.noreply.github.com>
2024-06-13 12:06:45 -0300

    v6

---
+++ TP FINAL/V6/main.asm
@@ -0,0 +1,109 @@
+.8086
+.model small
+.stack 100h
+.data
+	pedirNombre db "Ingrese su nombre.",0dh,0ah,24h
+	userName db 255 dup (24h),24h
+
+	eleccion db 0,24h
+
+	;adios db "Adios, ",0dh,0ah,24h
+	adios db "Adios, ",24h
+	gracias db ". Gracias por su tiempo.",0dh,0ah,24h
+
+
+
+.code
+
+;FUNCIONES PARA MENU + FUNCIONES GENERICAS. (todas en la libreria 'menu.asm')
+;================================
+	extrn cargaTexto:proc
+	extrn imprimirTexto:proc
+	extrn imprimirColor:proc
+	extrn menu:proc
+	extrn error:proc
+;================================
+
+
+;FUNCIONES EXTERNAS PARA EL JUEGO DE PIEDRA, PAPEL O TIJERA (en la libreria 'ppt.asm')
+;================================
+	extrn pptFunc:proc
+;================================
+
+
+;FUNCIONES EXTERNAS PARA EL JUEGO DE PIEDRA, PAPEL O TIJERA (en la libreria 'nums.asm')
+;================================
+	extrn numFunc:proc
+;================================
+
+
+	main proc
+		mov ax, @data
+		mov ds, ax
+
+		int 80h
+
+	;Carga de nombre
+		;---------------------------------------
+		lea si, pedirNombre
+		push si
+		call imprimirTexto
+
+		lea si,userName		;Guardamos el offset de la variable. 
+		push si				;Pusheamos el offset.
+		call cargaTexto		;Llamamos a la funcion para cargar una variable. (Con el offset pasado por stack).
+		;---------------------------------------
+
+
+	menuLabel:
+		;La funcion "menu" retorna un numero (la eleccion del usuario) en el registro 'AL'.
+		call menu
+		mov eleccion,al
+
+		int 80h
+
+	 	;Saltar a la eleccion del usuario.
+	 	cmp al,'0'
+	 	je finProc
+	 	cmp al,'1'
+	 	je ppt
+	 	cmp al,'2'
+	 	je num
+
+	 	;Si no se cumple ninguna comparacion anterior significa que el caracter ingresado no es reconocido como opcion. Por lo que se muestra un mensaje de error. 
+	 	call error
+		jmp menuLabel
+
+	ppt:
+	 	lea dx,userName		;La funcion pptFunc necesita que se haga push del offset de userName.
+	 	call pptFunc
+	 	jmp menuLabel
+
+	num:	
+		call numFunc
+		jmp menuLabel
+
+	 finProc:
+
+		;mov bl,0f0h	;left=background color ||	right=foreground color
+		;mov cl,06h	;Cantidad de caracteres.
+		;call imprimirColor
+
+		lea si, adios
+		push si
+		call imprimirTexto
+
+		lea si, userName
+		push si
+		call imprimirTexto
+
+		lea si, gracias
+		push si
+		call imprimirTexto
+
+		;------------
+		mov ax, 4c00h
+		int 21h
+
+	main endp
+end main
---
+++ TP FINAL/V6/readme.txt
@@ -0,0 +1,11 @@
+Para garantizar el funcionamiento correcto del programa, asegúrese de que todos los archivos estén en la carpeta correspondiente.
+
+Los archivos son:
+intLP.asm
+main.asm
+menu.asm
+ppt.asm
+nums.asm
+
+Adicionalmente, se cuenta con un archivo 'all.bat' que se encarga de instalar los complementos necesarios y ejecutar el programa.
+Individualmente, se cuenta con 'inst.bat' y 'run.bat', para intalar los complementos y ejecutar el programa respectivamente.
